81ac5c3e9dd631ade29a264a1073c0540aa474fd
bokismoki <borisbosnjak084@gmail.com>
2019-04-19 20:50:03 +0200

    Favison Template Recreation

---
+++ script.js
@@ -0,0 +1,119 @@
+// ONE
+document.getElementById("one").addEventListener("mouseenter", function () {
+
+    this.src = "img/1.png";
+
+});
+
+document.getElementById("one").addEventListener("mouseleave", function () {
+
+    this.src = "img/11.png";
+
+});
+
+// TWO
+document.getElementById("two").addEventListener("mouseenter", function () {
+
+    this.src = "img/2.png";
+
+});
+
+document.getElementById("two").addEventListener("mouseleave", function () {
+
+    this.src = "img/22.png";
+
+});
+
+// THREE
+document.getElementById("three").addEventListener("mouseenter", function () {
+
+    this.src = "img/3.png";
+
+});
+
+document.getElementById("three").addEventListener("mouseleave", function () {
+
+    this.src = "img/33.png";
+
+});
+
+// FOUR
+document.getElementById("four").addEventListener("mouseenter", function () {
+
+    this.src = "img/4.png";
+
+});
+
+document.getElementById("four").addEventListener("mouseleave", function () {
+
+    this.src = "img/44.png";
+
+});
+
+// FIVE
+document.getElementById("five").addEventListener("mouseenter", function () {
+
+    this.src = "img/5.png";
+
+});
+
+document.getElementById("five").addEventListener("mouseleave", function () {
+
+    this.src = "img/55.png";
+
+});
+
+
+
+
+// FACEBOOK
+document.getElementById("facebook").addEventListener("mouseenter", function(){
+
+    this.src = "img/facebook1.png";
+
+});
+
+document.getElementById("facebook").addEventListener("mouseleave", function(){
+
+    this.src = "img/facebook.png";
+
+});
+
+// TWITTER 
+document.getElementById("twitter").addEventListener("mouseenter", function(){
+
+    this.src = "img/twitter1.png";
+
+});
+
+document.getElementById("twitter").addEventListener("mouseleave", function(){
+
+    this.src = "img/twitter.png";
+
+});
+
+// INSTAGRAM
+document.getElementById("instagram").addEventListener("mouseenter", function(){
+
+    this.src = "img/instagram1.png";
+
+});
+
+document.getElementById("instagram").addEventListener("mouseleave", function(){
+
+    this.src = "img/instagram.png";
+
+});
+
+// YOUTUBE
+document.getElementById("youtube").addEventListener("mouseenter", function(){
+
+    this.src = "img/youtube1.png";
+
+});
+
+document.getElementById("youtube").addEventListener("mouseleave", function(){
+
+    this.src = "img/youtube.png";
+
+});
